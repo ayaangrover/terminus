f75cb6c86d9e89b73e041ad524489ec84051f12d
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-14 09:15:57 -0600

    Added SVG sanitization
    Necessary to allow SVGs to be sanitized. Supported is limited, though, because the Sanitize gem doesn't support attributes with mixed case. A good example is the SVG `viewBox` attribute.
    
    These changes are mostly a stop gap until a better solution can be put in place due to the limitations of the Sanitize gem.
    
    RuboCop was update to ignore the long class/method lengths, at least for now, in order to work on a better solution.
    
    Milestone: minor

--- app/aspects/sanitizer.rb
+++ app/aspects/sanitizer.rb
@@ -23,15 +23,176 @@ module Terminus
       def configuration = client::Config.merge(defaults, elements:, attributes:)
 
       def elements
-        defaults[:elements].including "canvas", "html", "link", "script", "source", "style"
+        defaults[:elements].including "canvas",
+                                      "circle",
+                                      "defs",
+                                      "ellipse",
+                                      "g",
+                                      "html",
+                                      "line",
+                                      "link",
+                                      "path",
+                                      "polygon",
+                                      "polyline",
+                                      "rect",
+                                      "script",
+                                      "source",
+                                      "style",
+                                      "svg",
+                                      "text",
+                                      "tspan"
       end
 
       def attributes
         defaults[:attributes].merge "canvas" => %w[id width height],
+                                    "circle" => %w[
+                                      cx
+                                      cv
+                                      r
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
                                     "div" => [:data],
+                                    "ellipse" => %w[
+                                      cx
+                                      cv
+                                      rx
+                                      rv
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
+                                    "g" => %w[stroke stroke-width fill transform],
+                                    "line" => %w[
+                                      x1
+                                      x2
+                                      v1
+                                      v2
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linecap
+                                      stroke-opacity
+                                      stroke-width
+                                      shape-rendering
+                                      transform
+                                    ],
                                     "link" => %w[href rel],
-                                    "script" => %w[src],
-                                    "source" => %w[type src srcset sizes media height width]
+                                    "path" => %w[
+                                      d
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linecap
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
+                                    "polygon" => %w[
+                                      points
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
+                                    "polyline" => %w[
+                                      points
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linecap
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
+                                    "rect" => %w[
+                                      x
+                                      y
+                                      width
+                                      height
+                                      rx
+                                      ry
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      shape-rendering
+                                      transform
+                                    ],
+                                    "script" => ["src"],
+                                    "source" => %w[type src srcset sizes media height width],
+                                    "svg" => %w[
+                                      height
+                                      width
+                                      x
+                                      y
+                                      version
+                                      shape-rendering
+                                    ],
+                                    "text" => %w[
+                                      x
+                                      y
+                                      dx
+                                      dy
+                                      rotate
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linecap
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                      transform
+                                    ],
+                                    "tspan" => %w[
+                                      x
+                                      y
+                                      dx
+                                      dy
+                                      rotate
+                                      stroke
+                                      stroke-dasharray
+                                      stroke-dashoffset
+                                      stroke-linecap
+                                      stroke-linejoin
+                                      stroke-opacity
+                                      stroke-width
+                                      fill
+                                      fill-opacity
+                                    ]
       end
     end
   end
