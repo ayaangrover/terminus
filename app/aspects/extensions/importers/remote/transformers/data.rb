736f95928ed6290cd29cc342462b9ae60f25e472
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-22 11:04:41 -0600

    Added extension remote import data transformer
    Ensures custom field defaults are provided as default data for immediate use in the Liquid template (and exchanges).
    
    Issue: 261
    Milestone: minor

---
+++ app/aspects/extensions/importers/remote/transformers/data.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "dry/core"
+require "dry/monads"
+require "initable"
+
+module Terminus
+  module Aspects
+    module Extensions
+      module Importers
+        module Remote
+          module Transformers
+            # Transforms custom field defaults into data defaults.
+            class Data
+              include Initable[target: :fields, keys: %w[keyname default]]
+              include Dry::Monads[:result]
+
+              def call attributes
+                data = attributes.fetch(target, Dry::Core::EMPTY_HASH)
+                                 .each
+                                 .with_object({}) do |item, all|
+                                   key, value = item.values_at(*keys)
+                                   all[key] = value if value
+                                 end
+
+                Success attributes.merge!(data:)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
